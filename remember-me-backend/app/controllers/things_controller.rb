6e7d155f26af77d8d59b73fe5f68c7bd2b15a61b
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-26 18:18:40 -0400

    Thing routes Built & tested fetching to index, show, update, create, and destroy routes for things

--- remember-me-backend/app/controllers/things_controller.rb
+++ remember-me-backend/app/controllers/things_controller.rb
@@ -1,2 +1,37 @@
 class ThingsController < ApplicationController
+    def index
+        render json: Thing.all
+    end
+
+    def show
+        render json: Thing.find(params[:id])
+    end
+
+    def create
+        thing = Thing.new(thing_params)
+        if thing.save
+            render json: thing
+        else
+            render json: thing.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        thing = Thing.find(params[:id])
+        if thing.update(thing_params)
+            render json: thing
+        else
+            render json: thing.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        Thing.find(params[:id]).destroy
+    end
+
+    private
+
+    def thing_params
+        params.require(:thing).permit(:name, :content, :image_url, :url, :is_favorite)
+    end
 end
