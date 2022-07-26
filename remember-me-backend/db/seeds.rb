0e811c02c608e975c5f6760d94d84d86d059743e
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-26 16:54:53 -0400

    initial seed

--- remember-me-backend/db/seeds.rb
+++ remember-me-backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cars = Thing.create(name: 'Cars', content: 'Never buy a car that is less than 3 years old.')
+civic = Thing.create(name: 'Blue Civic', content: 'My lovely 91 civic - such a moneypit')
+
+rel = ThingRelationship.create(parent_thing: cars, child_thing: civic)
