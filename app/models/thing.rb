b88e3af80a5c4366a32f5d9345cc3a4354e21e86
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-29 12:43:13 -0400

    destroy thing relationships before thing destruction

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,4 +1,13 @@
 class Thing < ApplicationRecord
     has_many :thing_relationships, foreign_key: :parent_thing_id
     has_many :child_things, through: :thing_relationships, source: :child_thing
+
+    before_destroy :destroy_relationships
+
+    private
+
+    def destroy_relationships
+        ThingRelationship.where(parent_thing_id: id).destroy_all
+        ThingRelationship.where(child_thing_id: id).destroy_all
+    end
 end
