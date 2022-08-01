42ebf8219f41c1260dc7c5aceba11e558ec4930b
Malcolm Keyes <3.malcolmk@gmail.com>
2022-08-01 13:02:15 -0400

    delete serializers

--- app/serializers/thing_relationship_serializer.rb
+++
@@ -1,3 +0,0 @@
-class ThingRelationshipSerializer < ActiveModel::Serializer
-    attributes :id, :parent_thing_id, :child_thing_id
-end
--- app/serializers/thing_serializer.rb
+++
@@ -1,4 +0,0 @@
-class ThingSerializer < ActiveModel::Serializer
-    attributes :id, :name, :content, :image_url, :url, :is_favorite, :updated_at
-    has_many :child_things
-end
